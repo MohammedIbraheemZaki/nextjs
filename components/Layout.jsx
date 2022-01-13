6d903c47e57d695cb325ba0c882fdc765a55d81d
MohammedIbraheemZaki <zakiz0054@gmail.com>
2022-01-13 14:30:23 +0200

    1- fix cross origin error

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,16 +1,17 @@
 import Head from 'next/head'
 import NavBar from './NavBar'
 import Footer from './Footer'
+import Script from 'next/script'
 const Layout = ({children}) => {
   return (
     <>
       <Head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
       </Head>
       <NavBar/>
         {children}
       <Footer/> 
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></Script>
     </>
   )
 }
